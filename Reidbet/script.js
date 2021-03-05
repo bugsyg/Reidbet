113ba761bcf6b4eaeab56014ee37c81fdbc3158c
bugsyg <78964358+bugsyg@users.noreply.github.com>
2021-03-05 11:20:18 -0300

    Add files via upload

---
+++ Reidbet/script.js
@@ -0,0 +1,13 @@
+let registro = document.getElementById("registrarse");
+let inicio = document.getElementById("Inicio");
+
+function cambiandoFormulario(){
+    registro.style.display = "none";
+    inicio.style.display = "block";
+}
+function cambiandoFormulario2() {
+    registro.style.display = "block";
+    inicio.style.display = "none";
+}
+
+
